bcb17b9be3a87f95154c0f1a6d469a9b3ef86d44
python237 <tagnedidier@gmail.com>
2024-05-31 00:53:03 +0100

    first commit

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+import os
+
+
+def home(request):
+	member1 = os.getenv("MEMBER_1")
+	member2 = os.getenv("MEMBER_2")
+	member3 = os.getenv("MEMBER_3")
+	team = os.getenv("TEAM")
+
+	return render(request, "home.html", locals())
